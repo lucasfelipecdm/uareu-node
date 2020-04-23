61fbed71d8385779b6c984415381bf623ab95583
Lucas Felipe Costa da Mata <lucasfelipecdm@hotmail.com>
2020-04-23 13:20:39 -0300

    :sparkles: Creates more array handler functions

--- src/modules/handlers/types/array/array.handler.ts
+++ src/modules/handlers/types/array/array.handler.ts
@@ -2,3 +2,9 @@ import * as ref from 'ref-napi';
 import * as ArrayType from 'ref-array-napi';
 
 export const CharArray = (length: number): typeof ArrayType => ArrayType(ref.types.char, length);
+
+export const UCharArray = (length: number): typeof ArrayType => ArrayType(ref.types.uchar, length);
+
+export const IntArray = (length: number): typeof ArrayType => ArrayType(ref.types.int, length);
+
+export const UIntArray = (length: number): typeof ArrayType => ArrayType(ref.types.uint, length);
