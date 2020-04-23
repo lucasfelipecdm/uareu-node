c4a0fa05664bce04480da2121b8c23950063bc3b
Lucas Felipe Costa da Mata <lucasfelipecdm@hotmail.com>
2020-04-23 13:19:59 -0300

    :white_check_mark: Creates more array handler tests

--- tests/modules/handlers/types/array/array.handler.test.ts
+++ tests/modules/handlers/types/array/array.handler.test.ts
@@ -1,9 +1,27 @@
-import { CharArray } from '../../../../../src/modules/handlers/types/array/array.handler';
+import { CharArray, UCharArray, IntArray, UIntArray } from '../../../../../src/modules/handlers/types/array/array.handler';
 import { MAX_STR_LENGTH } from '../../../../../src/modules/handlers/types/constant/constant.handler';
 import * as ref from 'ref-napi';
 
 test('Create CharArray using MAX_STR_LENGTH', () => {
-    const obj = CharArray(MAX_STR_LENGTH);
-    expect(obj.size).toBe(MAX_STR_LENGTH);
-    expect(obj.type).toBe(ref.types.char);
+    const array = CharArray(MAX_STR_LENGTH);
+    expect(array.size).toBe(MAX_STR_LENGTH);
+    expect(array.type).toBe(ref.types.char);
+});
+
+test('Create UCharArray using MAX_STR_LENGTH', () => {
+    const array = UCharArray(1);
+    expect(array.size).toBe(1);
+    expect(array.type).toBe(ref.types.uchar);
+});
+
+test('Create IntArray using MAX_STR_LENGTH', () => {
+    const array = IntArray(MAX_STR_LENGTH);
+    expect(array.size).toBe(MAX_STR_LENGTH * ref.types.int.size);
+    expect(array.type).toBe(ref.types.int);
+});
+
+test('Create UIntArray using MAX_STR_LENGTH', () => {
+    const array = UIntArray(1);
+    expect(array.size).toBe(ref.types.uint.size);
+    expect(array.type).toBe(ref.types.uint);
 });
