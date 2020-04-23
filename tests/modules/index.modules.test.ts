098748601c0c698a29863fed2d7e0fa037eb120e
Lucas Felipe Costa da Mata <lucasfelipecdm@hotmail.com>
2020-04-23 17:10:23 -0300

    :white_check_mark: Creates test of init and get version of UareU singleton class

--- tests/modules/index.modules.test.ts
+++ tests/modules/index.modules.test.ts
@@ -14,3 +14,20 @@ test('Get instance of UareU singleton class and load libs passing wrongs paths',
     const uareu = UareU.getInstance();
     await expect(uareu.loadLibs('binX/dpfpdd', 'binX/dpfj')).rejects.toThrow();
 });
+
+test('Get instance of UareU singleton class and load libs passing without paths', async () => {
+    const uareu = UareU.getInstance();
+    await expect(uareu.loadLibs()).resolves.toBe('SUCCESS');
+});
+
+test('Get DPFPDD Version using dpfpddVersion function from UareU class', async () => {
+    const uareu = UareU.getInstance();
+    await uareu.loadLibs('bin/dpfpdd', 'bin/dpfj');
+    await expect(uareu.dpfpddVersion()).rejects.toThrow();
+});
+
+test('Init DPFPDD using dpfpddInit function from UareU class', async () => {
+    const uareu = UareU.getInstance();
+    await uareu.loadLibs('bin/dpfpdd', 'bin/dpfj');
+    await expect(uareu.dpfpddInit()).resolves.toBe(0);
+});
