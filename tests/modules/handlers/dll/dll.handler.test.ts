5013a85bb138b4d94a5dc1bf07af4de907e98669
Lucas Felipe Costa da Mata <lucasfelipecdm@hotmail.com>
2020-04-23 15:55:22 -0300

    :white_check_mark: Add tests load from default path in dll load libs

--- tests/modules/handlers/dll/dll.handler.test.ts
+++ tests/modules/handlers/dll/dll.handler.test.ts
@@ -8,10 +8,18 @@ test('Loading of dpfpdd library with wrong path', async () => {
     await expect(DllHandler.loadDpfppdFrom('binX/dpfpdd')).rejects.toThrow('error');
 });
 
+test('Loading of dpfpdd library without a path', async () => {
+    await expect(DllHandler.loadDpfppdFrom()).resolves.not.toBeUndefined();
+});
+
 test('Loading of dpfj library with correct path', async () => {
     await expect(DllHandler.loadDpfjFrom('bin/dpfj')).resolves.not.toBeUndefined();
 });
 
 test('Loading of dpfj library with wrong path', async () => {
     await expect(DllHandler.loadDpfjFrom('binX/dpfj')).rejects.toThrow('error');
+});
+
+test('Loading of dpfj library without a path', async () => {
+    await expect(DllHandler.loadDpfjFrom()).resolves.not.toBeUndefined();
 });
