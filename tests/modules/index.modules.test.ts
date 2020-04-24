073fe1f2f5e283359c1b3fb05548769d963bf7be
Lucas Felipe Costa da Mata <lucasfelipecdm@hotmail.com>
2020-04-24 16:07:05 -0300

    :white_check_mark: Creates test to new functions of uareu singleton class

--- tests/modules/index.modules.test.ts
+++ tests/modules/index.modules.test.ts
@@ -1,4 +1,5 @@
 import UareU from '../../src/modules/';
+import { DPFPDD_PRIORITY } from '../../src/modules/handlers/types/constant/constant.handler';
 
 test('Get instance of UareU singleton class ', async () => {
     const uareu = UareU.getInstance();
@@ -20,7 +21,7 @@ test('Get instance of UareU singleton class and load libs passing without paths'
     await expect(uareu.loadLibs()).resolves.toBe('SUCCESS');
 });
 
-test('Get DPFPDD Version using dpfpddVersion function from UareU class', async () => {
+test('Try get DPFPDD Version using dpfpddVersion function from UareU class expect toThrow', async () => {
     const uareu = UareU.getInstance();
     await uareu.loadLibs('bin/dpfpdd', 'bin/dpfj');
     await expect(uareu.dpfpddVersion()).rejects.toThrow();
@@ -31,3 +32,51 @@ test('Init DPFPDD using dpfpddInit function from UareU class', async () => {
     await uareu.loadLibs('bin/dpfpdd', 'bin/dpfj');
     await expect(uareu.dpfpddInit()).resolves.toBe(0);
 });
+
+test('Init DPFPDD using dpfpddExit function from UareU class', async () => {
+    const uareu = UareU.getInstance();
+    await uareu.loadLibs('bin/dpfpdd', 'bin/dpfj');
+    await uareu.dpfpddInit();
+    await expect(uareu.dpfpddExit()).resolves.toBe(0);
+});
+
+test('Get list of reader devices connected using dpfpddQueryDevices function from UareU class', async () => {
+    const uareu = UareU.getInstance();
+    await uareu.loadLibs('bin/dpfpdd', 'bin/dpfj');
+    await uareu.dpfpddInit();
+    await expect(uareu.dpfpddQueryDevices()).resolves.not.toBeUndefined();
+});
+
+test('Open a reader using dpfpddOpen function from UareU class', async () => {
+    const uareu = UareU.getInstance();
+    await uareu.loadLibs('bin/dpfpdd', 'bin/dpfj');
+    await uareu.dpfpddInit();
+    const devices = await uareu.dpfpddQueryDevices();
+    await expect(uareu.dpfpddOpen(devices.devicesList[0])).resolves.not.toBeUndefined();
+});
+
+// test('Try open a reader using dpfpddOpenExt function from UareU class expecting to throw a error', async () => {
+//     const uareu = UareU.getInstance();
+//     await uareu.loadLibs('bin/dpfpdd', 'bin/dpfj');
+//     await uareu.dpfpddInit();
+//     const devices = await uareu.dpfpddQueryDevices();
+//     await expect(uareu.dpfpddOpenExt(devices.devicesList[0], DPFPDD_PRIORITY.DPFPDD_PRIORITY_EXCLUSIVE)).rejects.toThrow();
+// });
+
+// test('Close a reader using dpfpddClose function from UareU class', async () => {
+//     const uareu = UareU.getInstance();
+//     await uareu.loadLibs('bin/dpfpdd', 'bin/dpfj');
+//     await uareu.dpfpddInit();
+//     const devices = await uareu.dpfpddQueryDevices();
+//     const reader = await uareu.dpfpddOpen(devices.devicesList[0]);
+//     await expect(uareu.dpfpddClose(reader)).resolves.toBe(0);
+// });
+
+// test('Try open a reader using dpfpddGetDeviceStatus function from UareU class expecting to throw a error', async () => {
+//     const uareu = UareU.getInstance();
+//     await uareu.loadLibs('bin/dpfpdd', 'bin/dpfj');
+//     await uareu.dpfpddInit();
+//     const devices = await uareu.dpfpddQueryDevices();
+//     const reader = await uareu.dpfpddOpen(devices.devicesList[0]);
+//     await expect(uareu.dpfpddGetDeviceStatus(reader)).resolves.not.toBeUndefined();
+// });
